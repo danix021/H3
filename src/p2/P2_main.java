288170e8220e2de6638d9fd691f7c842553909be
danix021 <daniostadghadir@gmail.com>
2025-10-31 13:01:07 +0100

    aufgabe2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,17 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+		int i = 18;
+		boolean b = false;
+
+		if (i > 10) {
+			b = true;
+		}
+
+		System.out.println("i = " + i);
+		System.out.println("b = " + b);
+
+	}
+}
